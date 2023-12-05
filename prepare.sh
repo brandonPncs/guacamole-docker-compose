39ab438f08fd5adae816b1423d78c92b3cebcdb9
brandonPncs <153030490+brandonPncs@users.noreply.github.com>
2023-12-05 14:32:49 -0800

    Update prepare.sh
    prepares ssl and nginx folders on local host machine.

--- prepare.sh
+++ prepare.sh
@@ -6,11 +6,8 @@ then
 	echo "docker daemon not running, will exit here!"
 	exit
 fi
-echo "Preparing folder init and creating ./init/initdb.sql"
-mkdir ./init >/dev/null 2>&1
+echo "Preparing folder nginx ssl and creating guacamole container"
 mkdir -p ./nginx/ssl >/dev/null 2>&1
-chmod -R +x ./init
-docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
 echo "done"
 echo "Creating SSL certificates"
 openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
